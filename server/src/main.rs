42d4d1a830eaebac600cf424e7447b8ab2b4c255
Koki <kokiidoki@gmail.com>
2024-01-02 10:37:42 -0500

    working image send over tcp

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,28 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, fs::File, io::{Read, Write, Result, self}};
 
-fn main() -> std::io::Result<()> {
+fn main() -> io::Result<()> {
+    // let path_as_argument = std::env::args()
+    // .nth(1)
+    // .expect("File must be provided to send to clients");
+    
+    // send_bun_to_clients(&path_as_argument)
+
+    // TEST
+    send_bun_to_clients("test/dumpling.jpg")
+}
+
+fn send_bun_to_clients(image_path: &str) -> Result<()> {
+    let mut send_file = File::open(image_path)?;
+    let mut file_buffer = Vec::new();
+    send_file.read_to_end(&mut file_buffer)?;
+
     let listener = TcpListener::bind("127.0.0.1:3523")?;
-    dbg!(listener.local_addr());
-    for stream in listener.incoming().flatten() {
+    for mut stream in listener.incoming().flatten() {
         dbg!(&stream);
-        // stream.shutdown(std::net::Shutdown::Both)?;
+        stream.write_all(&file_buffer.clone())?;
+        stream.flush()?;
+        dbg!("write done");
     }
+
     Ok(())
 }

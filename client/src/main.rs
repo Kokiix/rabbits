fba12d0ac86a1d541aa66354ce09cb19e3cfde7d
Koki <kokiidoki@gmail.com>
2024-01-02 13:17:27 -0500

    schedule timing between server and clients

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,29 +1,33 @@
-use std::{thread, time::{Duration}, net::TcpStream, fs::self, io::Read};
+use std::{thread, time::Duration, net::TcpStream, io::{Read, self, Result}, fs};
 use chrono::offset::Local;
 
-fn main() {
-    let ping_frequency = Duration::from_secs(10);
+const PING_SEC_FREQUENCY: u64 = 5;
+
+fn main() -> io::Result<()> {
+    let ping_frequency = Duration::from_secs(PING_SEC_FREQUENCY);
     loop {
         let attempted_server_connection = TcpStream::connect("67.8.144.213:3523");
         if let Ok(mut connection) = attempted_server_connection {
             let mut file_buffer = Vec::new();
-            connection.read_to_end(&mut file_buffer);
+            connection.read_to_end(&mut file_buffer)?;
 
-            write_to_file(file_buffer);
-            break;
+            write_to_file(file_buffer)?;
+            thread::sleep(ping_frequency);
         }
 
         thread::sleep(ping_frequency);
     }
 }
 
-fn write_to_file(file_buffer: Vec<u8>) {
+fn write_to_file(file_buffer: Vec<u8>) -> Result<()> {
     let time_of_read = Local::now().naive_local().format("%Y_%m_%d--%H_%M");
     let file_name = format!("rabbitdl-{time_of_read}.jpg");
 
     let dir_path = "C:\\ProgramData\\Rabbits";
     let file_path = format!("{dir_path}\\{file_name}");
 
-    fs::create_dir(dir_path);        
-    fs::write(file_path, file_buffer);
+    fs::create_dir(dir_path);
+    fs::write(file_path, file_buffer)?;
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,16 @@
-use std::{net::TcpListener, fs::File, io::{Read, Write, Result, self}};
+use std::{net::TcpListener, fs::File, io::{Read, Write, Result, self}, time::{SystemTime, UNIX_EPOCH, Duration}, thread};
+
+const PING_SEC_FREQUENCY: u64 = 5;
 
 fn main() -> io::Result<()> {
-    // let path_as_argument = std::env::args()
-    // .nth(1)
-    // .expect("File must be provided to send to clients");
+    let path_as_argument = std::env::args()
+    .nth(1)
+    .expect("File must be provided to send to clients");
     
-    // send_bun_to_clients(&path_as_argument)
+    send_bun_to_clients(&path_as_argument)
 
     // TEST
-    send_bun_to_clients("test/dumpling.jpg")
+    // send_bun_to_clients("test/dumpling.jpg")
 }
 
 fn send_bun_to_clients(image_path: &str) -> Result<()> {
@@ -17,11 +19,28 @@ fn send_bun_to_clients(image_path: &str) -> Result<()> {
     send_file.read_to_end(&mut file_buffer)?;
 
     let listener = TcpListener::bind("192.168.0.152:3523")?;
-    for mut stream in listener.incoming().flatten() {
-        dbg!(&stream);
-        stream.write_all(&file_buffer.clone())?;
-        stream.flush()?;
-        dbg!("write done");
+    listener.set_nonblocking(true)?;
+
+    let mut seconds_without_connection = 0;
+
+    for potential_connection in listener.incoming() {
+        if seconds_without_connection == PING_SEC_FREQUENCY {
+            break;
+        }
+
+        match potential_connection {
+            Ok(mut connection) => {
+                dbg!(&connection);
+                connection.write_all(&file_buffer.clone())?;
+                connection.flush()?;
+                seconds_without_connection = 0;
+            },
+            Err(_) => {
+                seconds_without_connection += 1;
+            }
+        }
+
+        thread::sleep(Duration::from_secs(1));
     }
 
     Ok(())

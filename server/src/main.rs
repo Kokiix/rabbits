79ba7403b827878c26170417c5c8795688b3be56
Koki <kokiidoki@gmail.com>
2024-01-03 09:37:20 -0500

    BEEG refactor

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,35 +1,64 @@
-use std::{net::TcpListener, fs::File, io::{Read, Write, Result}, time::Duration, thread};
+use std::{
+    net::TcpListener, 
+    
+    io::{Read, Write, Result}, 
+    fs::File,
+
+    time::Duration, 
+
+    thread, 
+    panic
+};
 
 const PING_FREQ_MS: u64 = 5000;
 const LISTENER_FREQ_MS: u64 = 5;
+const CLI_USAGE: &str = "RABBITS: Rapid Automated BunBun Image Transmission System\n\nUsage:\nrabbits -c --count-clients\nrabbits <file>\n\nOptions:\n-c --count-clients     count the number of active clients\n\n";
 
 fn main() {
-    let path_as_argument = std::env::args()
-    .nth(1);
-    if path_as_argument.is_none() {
-        println!("Usage: rabbits <file>");
-        return;
-    }
-    
-    send_bun_to_clients(&path_as_argument.unwrap()).unwrap();
+    set_panic_message();
 
-    // TEST
-    // send_bun_to_clients("test/dumpling.png")
+    let mut args = std::env::args();
+    if args.len() == 2 {
+        let first_arg = args.nth(1).unwrap();
+        match first_arg.as_str() {
+            "-c" | "--count-clients" => {
+                // TODO
+            },
+            first_arg => {
+                let file_open_attempt = File::open(first_arg);
+                match file_open_attempt {
+                    Ok(mut file) => {
+                        let mut file_buffer = Vec::new();
+                        file.read_to_end(&mut file_buffer).expect("file read error");
+
+                        open_client_listener(file_buffer).expect("connection error");
+                    },
+                    Err(_) => panic!("invalid file path\nTry 'rabbits' for more information."),
+                }
+            }
+        };
+    } else {
+        println!("{CLI_USAGE}");
+    }
 }
 
-fn send_bun_to_clients(image_path: &str) -> Result<()> {
-    let mut send_file = File::open(image_path)?;
-    let mut file_buffer = Vec::new();
-    send_file.read_to_end(&mut file_buffer)?;
-
+fn open_client_listener(file_buffer: Vec<u8>) -> Result<()> {
     let listener = TcpListener::bind("192.168.0.152:3523")?;
     listener.set_nonblocking(true)?;
 
-    let mut ms_to_timeout: u64 = 0;
+    let connection_count = handle_incoming_connections(listener, file_buffer)?;
+
+    println!("Image sent to {} clients", connection_count);
+
+    Ok(())
+}
+
+fn handle_incoming_connections(listener: TcpListener, file_buffer: Vec<u8>) -> Result<usize> {
+    let mut timeout_counter: u64 = 0;
     let mut connection_count = 0;
 
     for potential_connection in listener.incoming() {
-        if ms_to_timeout == PING_FREQ_MS {
+        if timeout_counter == PING_FREQ_MS {
             break;
         }
 
@@ -38,13 +67,22 @@ fn send_bun_to_clients(image_path: &str) -> Result<()> {
             connection.write_all(&file_buffer.clone())?;
             connection.flush()?;
         } else {
-            ms_to_timeout += LISTENER_FREQ_MS;
+            timeout_counter += LISTENER_FREQ_MS;
         }
 
         thread::sleep(Duration::from_millis(LISTENER_FREQ_MS));
     }
 
-    println!("Sent to {} clients", connection_count);
+    Ok(connection_count)
+}
 
-    Ok(())
+fn set_panic_message() {
+    panic::set_hook(Box::new(|panic_info| {
+        // WORKAROUND:
+        // message func is still nightly for panic info,
+        // because of having to support older versions of core::panic!()
+        let panic_str = format!("{panic_info}");
+        let panic_message = panic_str.split('\n').nth(1).unwrap();
+        println!("rabbits: {panic_message}");
+    }));
 }

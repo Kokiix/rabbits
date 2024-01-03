5a130b5fdb1f5979beea455e60d0b21f425b7f33
Koki <kokiidoki@gmail.com>
2024-01-02 22:08:22 -0500

    adjust printed output

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,17 +1,20 @@
-use std::{net::TcpListener, fs::File, io::{Read, Write, Result, self}, time::Duration, thread};
+use std::{net::TcpListener, fs::File, io::{Read, Write, Result}, time::Duration, thread};
 
-const PING_FREQ_MS: u64 = 1000;
+const PING_FREQ_MS: u64 = 5000;
 const LISTENER_FREQ_MS: u64 = 5;
 
-fn main() -> io::Result<()> {
-    // let path_as_argument = std::env::args()
-    // .nth(1)
-    // .expect("File must be provided to send to clients");
+fn main() {
+    let path_as_argument = std::env::args()
+    .nth(1);
+    if path_as_argument.is_none() {
+        println!("Usage: rabbits <file>");
+        return;
+    }
     
-    // send_bun_to_clients(&path_as_argument)
+    send_bun_to_clients(&path_as_argument.unwrap()).unwrap();
 
     // TEST
-    send_bun_to_clients("test/dumpling.png")
+    // send_bun_to_clients("test/dumpling.png")
 }
 
 fn send_bun_to_clients(image_path: &str) -> Result<()> {
@@ -23,6 +26,7 @@ fn send_bun_to_clients(image_path: &str) -> Result<()> {
     listener.set_nonblocking(true)?;
 
     let mut ms_to_timeout: u64 = 0;
+    let mut connection_count = 0;
 
     for potential_connection in listener.incoming() {
         if ms_to_timeout == PING_FREQ_MS {
@@ -30,17 +34,17 @@ fn send_bun_to_clients(image_path: &str) -> Result<()> {
         }
 
         if let Ok(mut connection) = potential_connection {
-            dbg!(&connection);
+            connection_count += 1;
             connection.write_all(&file_buffer.clone())?;
             connection.flush()?;
         } else {
             ms_to_timeout += LISTENER_FREQ_MS;
         }
 
-        dbg!(ms_to_timeout);
-
         thread::sleep(Duration::from_millis(LISTENER_FREQ_MS));
     }
+
+    println!("Sent to {} clients", connection_count);
 
     Ok(())
 }
